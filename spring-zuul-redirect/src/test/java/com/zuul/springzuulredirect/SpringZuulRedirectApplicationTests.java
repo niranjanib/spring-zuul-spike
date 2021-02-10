db2becd6d77ce6d44508c81feb437dc411eb0cab
niranjanib <niranjani_b@live.com>
2021-02-10 19:12:13 +0530

    add zuul proxy application

---
+++ spring-zuul-redirect/src/test/java/com/zuul/springzuulredirect/SpringZuulRedirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.springzuulredirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZuulRedirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
